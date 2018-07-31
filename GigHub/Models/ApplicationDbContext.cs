0a21be4982a65d12ce18f2c3a4d29f4cb18c4d1f
mattnadd <35609305+mattnadd@users.noreply.github.com>
2018-07-31 13:19:30 -0400

    Updated API

---
+++ GigHub/Models/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.Entity;
+using System.Security.AccessControl;
+using System.Web.Profile;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace GigHub.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+
+    {
+        public DbSet<Gig> Gigs { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Attendance>()
+                .HasRequired(a => a.Gig).WithMany()
+                .WillCascadeOnDelete(false);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
+
